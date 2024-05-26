53cf042092424a0629a617d68d226fef21ff47af
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-05-26 22:30:34 +0200

    working on displaying fetched data

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const API_KEY = "81efd49b0be053aecd506b5fb8a8d754";
-const BASE_URL_API = "https://api.openweathermap.org/data/2.5/weather";
+const BASE_API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 const content = {
+  mainGrid: document.querySelector(".grid-item:first-child"),
   locationName: document.querySelector(".location"),
   temp: document.querySelector(".temp"),
   unit: document.querySelector(".unit"),
@@ -14,6 +15,7 @@ const content = {
   humidity: document.querySelector(".humidity"),
   windSpeed: document.querySelector(".wind-speed"),
   pressure: document.querySelector(".pressure"),
+  weatherImage: document.querySelector(".weather-image"),
 };
 
 const searchForm = document.querySelector(".search-form");
@@ -23,38 +25,39 @@ async function onSearchFormSubmit(e) {
   e.preventDefault();
 
   const locationName = searchForm.search.value.trim();
-
   if (!locationName) {
     alert("Please enter a city, state or country name.");
     return;
   }
+
   const isImperial = document.getElementById("unitType").checked === false;
 
   const unitType = isImperial ? "imperial" : "metric";
 
   try {
     const data = await getWeatherByLocation(locationName, unitType);
+
     if (data.cod === "404") {
-      alert("Location Not Found");
+      alert("Location not found.");
       return;
     }
 
     displayWeatherData(data, isImperial);
   } catch (error) {
-    console.error("Error on form submit", error);
+    console.error("Error on form submit.", error);
   }
 
   searchForm.reset();
 }
 
 async function getWeatherByLocation(locationName, unitType) {
-  const apiUrl = `${BASE_URL_API}?q=${locationName}&appid=${API_KEY}&units=${unitType}`;
+  const apiUrl = `${BASE_API_URL}?q=${locationName}&appid=${API_KEY}&units=${unitType}`;
 
-  return await fetch(apiUrl);
+  return await fetchData(apiUrl);
 }
 
 async function getWeatherByPosition(lat, lon, unitType) {
-  const apiUrl = `${BASE_URL_API}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${unitType}`;
+  const apiUrl = `${BASE_API_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${unitType}`;
 
   return await fetchData(apiUrl);
 }
@@ -70,8 +73,15 @@ async function fetchData(apiUrl) {
 }
 
 function displayWeatherData(data, isImperial) {
+  const { weather, main, wind, sys, name } = data;
+  console.log(data);
+
   // set units
   const tempUnit = isImperial ? "°F" : "°C";
   const windSpeedUnit = isImperial ? "mph" : "m/s";
   const pressureUnit = isImperial ? "inHg" : "hPa";
+
+  // set data
+  // content.locationName.textContent = name;
+  // content.temp.textContent = main.temp;
 }
